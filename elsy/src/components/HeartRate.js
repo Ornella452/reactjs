2303c310413688a584ac5a1569987be36f285c2c
ourfi <Ornella93@outlook.fr>
2020-09-17 01:21:43 +0200

    exo jour1 terminer 01: 21

--- elsy/src/components/HeartRate.js
+++ elsy/src/components/HeartRate.js
@@ -4,10 +4,10 @@ class App extends React.Component {
     render(){
         return(
 
-           < div class = "box col-md-2 col-6"  style={{color: 'red'}}> 
-         
-            <Icon name="favorite"></Icon>
-            <p>Heart : {this.props.heart}</p>
+           < div class = "box col-md-2 col-6"> 
+         <span> <Icon name="favorite"></Icon></span>
+           
+            <p  style={{color: 'black'}}>Heart : {this.props.heart}</p>
            
            </div>
 
