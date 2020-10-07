6b2e184985cc484dfe0c1fc33a3214929d01b75f
ourfi <Ornella93@outlook.fr>
2020-10-07 16:52:36 +0200

    exo moovice

--- react_moovice/src/App.js
+++ react_moovice/src/App.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Discover from './components/Discover';
+import DiscoverBattle from './components/DiscoverBattle';
+import MyList from './components/MyList';
+import Popular from './components/Popular';
+import PopularBattle from './components/PopularBattle';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <nav>
+            <ul>
+              <li><Link to="/">This week </Link></li>
+              <li><Link to="/battle/">This week battle</Link></li>
+              <li><Link to="/popular/">Popular</Link></li>
+              <li><Link to="/popular-battle/">Popular Battle</Link></li>
+              <li><Link to="/my-list/">My List</Link></li>
+
+            </ul>
+          </nav>
+          <Switch>
+
+            <Route path="/battle/"><DiscoverBattle /></Route>
+            <Route path="/popular/"><Popular /></Route>
+            <Route path="/popular-battle/"><PopularBattle /></Route>
+            <Route path="/my-list/"><MyList /></Route>
+            <Route path="/"><Discover /></Route>
+
+
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ react_moovice/src/components/Discover.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Discover extends React.Component {
+    render(){
+        return(
+            <div>This week
+            </div>
+        )
+    }
+}
+
+export default Discover;
