60295fb51678fd1cf94c94f317899d55eb119fb3
ourfi <Ornella93@outlook.fr>
2020-10-15 16:58:11 +0200

    exo etape 1 react_trippy

---
+++ react_trippy/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import HomeContainer from './containers/HomeContainer'
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <Switch>
+              <Route path="/">
+                <HomeContainer />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react_trippy/src/components/City.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class City extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+              
+                
+            </div>
+        )
+    }
+    }
+
+export default City
