abf0f0ec5e1d7fa1643e2bd992e4af409d1105fb
ourfi <Ornella93@outlook.fr>
2020-10-20 22:20:39 +0200

    exercice pokedex finish

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,46 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
-import Pokemon from './components/Pokemon';
-import MenuPokemon from './components/MenuPokemon';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
-      name: "",
-      height: 0,
-      weight: 0,
-      type: "",
-      src: "",
-
+      pokelist: [],
+      currentPokemon: null,
+      img: ""
     }
+    this.pokedex = this.pokedex.bind(this)
   }
+
   componentDidMount() {
 
-    this.pokedex('1')
+    this.PokemonList()
   }
 
-  pokedex(id) {
 
-    fetch(id)
-      .then(res => res.json())
-      .then(json => {
-        this.setState({
-          name: json.name,
-          height: json.height,
-          weight: json.weight,
-          type: json.types[0].type.name,
-          src: `https://pokeres.bastionbot.org/images/pokemon/${id}.png`,
-        
-        })// end setState
 
-      })
-  }
-  
- /* PokemonList(){
+  PokemonList(){
 
- 
+  // recupérerv les 25 pokemon
     fetch('https://pokeapi.co/api/v2/pokemon/?limit=25')
     .then(response => response.json())
     .then(json => {
@@ -53,26 +37,58 @@ class App extends Component {
       console.log('heyy', json.results)
     })
   }
-*/
+
+
+  pokedex(id) {
+
+    fetch(id)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          img: `https://pokeres.bastionbot.org/images/pokemon/${json.id}.png`,
+          currentPokemon: json
+        
+        })// end setState
+
+      })
+  }
+  
+  
   render() {
     return (
-
-      
-
       <div className="App">
 
-        <Pokemon name={this.state.name}
-          height={this.state.height}
-          weight={this.state.weight}
-          type={this.state.type}
-          src={this.state.src} />
+<div className="App">
+        <section className="single-poke d-block p-2 bg-dark text-white">
+          {this.state.currentPokemon ?
+            (<div className="poke-info text-center">
+              <img className="col-md-3 col-sm-6 mb-5" src={this.state.img}></img>
+              <div className="poke-info position-relative">
+                <p><b>Name :</b> {this.state.currentPokemon.name}</p>
+                <p><b>Height :</b>{this.state.currentPokemon.height}</p>
+                <p><b>Weight :</b>{this.state.currentPokemon.weight}</p>
+                <p><b>Type :</b>{this.state.currentPokemon.types[0].type.name}</p>
 
-          
-
-<MenuPokemon onClick={() => this.pokedex('1')}><img src="https://pokeres.bastionbot.org/images/pokemon/1.png" alt="pokemon" /></MenuPokemon>
-<MenuPokemon onClick={() => this.pokedex('2')}><img src="https://pokeres.bastionbot.org/images/pokemon/2.png" alt="pokemon" /></MenuPokemon>
-
-    
+              </div>
+            </div>) :
+            (<h1>Please select a pokemon</h1>)
+          }
+        </section>
+        <section className="row">
+          {this.state.pokelist.map((elem, index) => {
+            return (
+              //list de pokemon
+              <div onClick={() => this.pokedex(elem.url)} className="card shadow p-4 mb-2 bg-white col-md-3 col-sm-6 mb-5">
+                <div className="">
+                <img src={`https://pokeres.bastionbot.org/images/pokemon/${index + 1}.png`}  className="col-md-3 col-sm-6 mb-5"></img>
+                <p>{elem.name}</p>
+                </div>
+              </div>
+              
+            )
+          })}
+        </section>
+      </div>
 
       </div>
 
@@ -82,3 +98,5 @@ class App extends Component {
 }
 
 export default App;
+
+
